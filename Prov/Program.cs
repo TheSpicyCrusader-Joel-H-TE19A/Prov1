77c538f826a705b46f22d6479881da991d9d8d32
Joel <joel.herfindal@learnet.se>
2020-10-05 08:38:19 +0200

    Prov start

---
+++ Prov/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string coordinates = "7";
+            Console.WriteLine("Welcome to another episode of Bomb That Ship!");
+            Console.ReadLine();
+            Console.WriteLine("Your goal as the player is to try to hit the enemy Ship. Its coordinates is between 1,0 and 10,0 on your coordinated.");
+            Console.ReadLine();
+            Console.WriteLine("So basically you have to guess which coordinates it is on, between the number 1 and 10"):
+            Console.ReadLine();
+            Console.WriteLine("So lets get started!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
